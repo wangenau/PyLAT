4b79af7555e9776ba03e4faeb3c5f865529de8a4
Wanja Schulze <wangenau@protonmail.com>
2024-04-29 11:42:31 +0200

    Adjust examples namespace

--- Examples/Acetonitrile/visc/inputgenerator.py
+++ Examples/Acetonitrile/visc/inputgenerator.py
@@ -9,15 +9,19 @@ import os
 import random
 
 
-samplefile = open('test_1/in.visc','r').readlines()
-filelist= range(2,51)
+samplefile = open("test_1/in.visc", "r").readlines()
+filelist = range(2, 51)
 for num in filelist:
-    os.system('mkdir test_{0}'.format(num))
-    output = open('test_{0}/in.visc'.format(num),'w')
-    for line in range(0,len(samplefile)):
+    os.system("mkdir test_{0}".format(num))
+    output = open("test_{0}/in.visc".format(num), "w")
+    for line in range(0, len(samplefile)):
         if line == 31:
-            output.write('velocity        all create  ${{mytemp}} {0} units box \n'.format(random.randint(1,999999999)))
+            output.write(
+                "velocity        all create  ${{mytemp}} {0} units box \n".format(
+                    random.randint(1, 999999999)
+                )
+            )
         else:
             output.write(samplefile[line])
     output.close()
-    os.system('cp test_1/mol.data test_{0}/'.format(num))
+    os.system("cp test_1/mol.data test_{0}/".format(num))
