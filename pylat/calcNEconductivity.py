711277b53c3c59cccabc5e42afc5d5742fae7a1a
Wanja Schulze <wangenau@protonmail.com>
2025-11-12 14:06:25 +0100

    Implement the optional Yeh Hummer Correction in Conductivity calculations

--- pylat/calcNEconductivity.py
+++ pylat/calcNEconductivity.py
@@ -19,30 +19,54 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 
+import math
+
 
 class calcNEconductivity:
-    def calcNEconductivity(self, output, molcharge, Lx, Ly, Lz, nummoltype, moltypel, T):
+    def calcNEconductivity(self, output, molcharge, Lx, Ly, Lz, nummoltype, moltypel, T, nu=None):
         """This function uses the Nernst-Einstein equation to estimate the ionic
         conductivity of the system from the diffusivities
 
         """
+
+        output["Conductivity"] = {}
         V = Lx * Ly * Lz * 10**-30
         e = 1.60217657e-19
         k = 1.3806488e-23
+        if nu is not None:
+            xi = 2.837298
+            assert abs(Lx - Ly) < 1e-6
+            assert abs(Lx - Lz) < 1e-6
+            corr = k * T * xi / (math.pi * 6 * nu * 1e-3 * Lx * 1e-10)
+            output["Diffusivity Corrected"] = {}
+            output["Diffusivity Corrected"]["Yeh_Hummer_Correction"] = corr
+            output["Conductivity Corrected"] = {}
+
         NEcond = 0
+        NEcond_corr = 0
         for i in range(len(moltypel)):
             q = float(molcharge[moltypel[i]])
             if q != 0:
                 try:
                     D = float(output["Diffusivity"][moltypel[i]])
+                    if nu is not None:
+                        D_corr = D + corr
+                        output["Diffusivity Corrected"][moltypel[i]] = D_corr
                 except ValueError:
                     output["Nernst Einstein Conductivity in S/m"] = "runtime not long enough"
                     return output
                 N = int(nummoltype[i])
                 NEcond += N * q**2 * D
                 output["Conductivity"][f"Nernst_Einstein_{moltypel[i]}"] = N * q**2 * D * e**2 / k / T / V
+                if nu is not None:
+                    NEcond_corr += N * q**2 * D_corr
+                    output["Conductivity Corrected"][f"Nernst_Einstein_{moltypel[i]}"] = (
+                        N * q**2 * D_corr * e**2 / k / T / V
+                    )
         NEcond *= e**2 / k / T / V
-
         output["Conductivity"]["Nernst_Einstein"] = NEcond
+        if nu is not None:
+            NEcond_corr *= e**2 / k / T / V
+            output["Conductivity Corrected"]["Nernst_Einstein"] = NEcond_corr
 
         return output
